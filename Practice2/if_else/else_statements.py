7b333a0ef4dc46de1325b071246e182286615a16
Suanbek Quralai <kuralaiisuanbek@gmail.com>
2026-02-07 10:27:53 +0500

    Add examples of Python else statements

---
+++ Practice2/if_else/else_statements.py
@@ -0,0 +1,58 @@
+#Python Else Statements
+
+#example1
+a = 200
+b = 33
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+
+#example2
+a = 200
+b = 33
+
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+
+
+#example3
+a = 200
+b = 300
+
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+
+
+#example4
+a = 200
+b = 200
+
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+
+#example5
+a = 50
+b = 33
+
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+
