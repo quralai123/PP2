6c1874197c7e7e615f172ceeb0986843d06fb0ad
Suanbek Quralai <kuralaiisuanbek@gmail.com>
2026-02-28 08:27:04 +0500

    Add Practice4 - Python iterators, generators, dates, math, and JSON

--- Practice4/exercises/json_ex/json_ex.py
+++ Practice4/exercises/json_ex/json_ex.py
@@ -1,19 +1,20 @@
 import json
 
-with open("sample-data.json") as file:
-    data = json.load(file)
+# loading json= data from file
+with open("sample-data.json") as f:
+    data = json.load(f)
 
+# header
 print("Interface Status")
 print("=" * 80)
-print(f"{'DN':50} {'Description':20} {'Speed':8} {'MTU':6}")
+print("{:<50} {:<20} {:<8} {:<6}".format("DN", "Description", "Speed", "MTU"))
 print("-" * 80)
 
+# iterate through json0 and extract values
 for item in data["imdata"]:
     attributes = item["l1PhysIf"]["attributes"]
-    
-    dn = attributes["dn"]
-    descr = attributes["descr"]
-    speed = attributes["speed"]
-    mtu = attributes["mtu"]
-    
-    print(f"{dn:50} {descr:20} {speed:8} {mtu:6}")
+    dn = attributes.get("dn", "")
+    descr = attributes.get("descr", "")
+    speed = attributes.get("speed", "")
+    mtu = attributes.get("mtu", "")
+    print("{:<50} {:<20} {:<8} {:<6}".format(dn, descr, speed, mtu))
